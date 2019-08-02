22651435beaee6254151d16580037c87cb59b142
intel <intel@DESKTOP-MG3794A>
2019-08-02 16:05:32 +0530

    ds

--- springRestProject2/src/main/java/com/shyam/spring/rest/HelloController.java
+++ springRestProject2/src/main/java/com/shyam/spring/rest/HelloController.java
@@ -19,7 +19,7 @@ import com.shyam.spring.beans.TeamMember;
 public class HelloController {
 	@RequestMapping(method=RequestMethod.GET , path="/hello")
 	public String sayHello() {
-		return "Wecome ="+"name";
+		return "Wecome ="+"name1";
 	}
 	
 	//Example of Getmapping and @RequestParam
